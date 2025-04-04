2b901dcf8a03ed5faa5609aa0b39136244bfa9a3
André Maurilio <117940329+AndreMaurilioDEV@users.noreply.github.com>
2025-04-04 15:54:38 -0300

    Aula 03 dia 04/04

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from src.extractTransform import trimestreFunc
 from src.load import salvarCsv
 import pandas as pd
+from src.load import salvarSqlite
+from src.load import salvarMySQL
 
-dadosBcb = trimestreFunc('20231')
-salvarCsv(dadosBcb, 'src/datasets/meiosPagamentosTri.csv', ';', '.')
+dadosBcb = trimestreFunc("20231")
 
+# salvarCsv(dadosBcb, 'ETL-BCB/src/datasets/meiosPagamentosTri.csv', ';', '.')
 
+# salvarSqlite(dadosBcb, "src/datasets/etlbcb.db", "meios_pagamentos_tri")
 
+salvarMySQL(dadosBcb, "root", "teste", "localhost", "etlbcb", "meios_pagamentos_tri")
